ca5c7a4f4aee25d92146a516f644416e3627c263
falicov <121999608+falicov@users.noreply.github.com>
2026-05-15 23:53:54 -0400

    getting a weird error, stopping for the night
    seems like GameManager is not instantiating variables right?

--- Scripts/buyable_or_sellable_button.gd
+++ Scripts/buyable_or_sellable_button.gd
@@ -1,13 +1,20 @@
-extends Button
+extends Panel
 
 @export var resourceName:String;
 @export var amountDisplay:Label;
 @export var priceDisplay:Label;
+@export var nameDisplay:Label;
 
 func _ready() -> void:
 	amountDisplay.text = str(0);
-	priceDisplay.text = GameManager
+	print(GameManager.resourceDict[resourceName])
+	priceDisplay.text = "$"+str(GameManager.resourceDict[resourceName].value)
+	nameDisplay.text = resourceName.capitalize()
 
 func increment() -> void:
 	amountDisplay.text = str(GameManager.incrementResource(resourceName))
+	pass
+
+func decrement() -> void:
+	amountDisplay.text = str(GameManager.decrementResource(resourceName))
 	pass
