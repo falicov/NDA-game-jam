e4b629fd1e2be91426f8eff1fd4cc2b39699037f
falicov <121999608+falicov@users.noreply.github.com>
2026-05-16 12:17:05 -0400

    officially u can buy and sell stuff!!!!

--- screens/money_label.gd
+++ screens/money_label.gd
@@ -1,11 +1,17 @@
 extends Label
 
+var money:int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	money = GameManager.money
+	text = "$" + str(money)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if(GameManager.money != money):
+		money = GameManager.money
+		text = "$" + str(money)
+	return
