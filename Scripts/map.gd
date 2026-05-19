50ab7cb7d2a440295a2bfeda1ea475c666b7ee41
falicov <121999608+falicov@users.noreply.github.com>
2026-05-19 18:11:06 -0400

    started tilemap stuff at long last

--- Scripts/map.gd
+++ Scripts/map.gd
@@ -1,19 +1,8 @@
 extends TileMapLayer
 
-class _tile:
-	var coal
-	var iron
-	var copper
-	
-	func _init(_coal:int, _iron:int, _copper:int):
-		coal = _coal
-		iron = _iron
-		copper = _copper
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	
 	pass # Replace with function body.
 
 
